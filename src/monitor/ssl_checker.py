159cb3a1e7f20b97ed32fabac807235e49c2a08e
Joan Marc Riera Duocastella <mriera@ebi.ac.uk>
2026-03-05 00:25:45 +0000

    chore(task): Complete task 004 - ssl-checker

---
+++ src/monitor/ssl_checker.py
@@ -0,0 +1,49 @@
+import ssl
+import socket
+from datetime import datetime, timedelta
+from typing import Dict
+
+def check_ssl(hostname: str, port: int = 443) -> Dict:
+    try:
+        context = ssl.create_default_context()
+        with socket.create_connection((hostname, port)) as sock:
+            with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+                cert = ssock.getpeercert()
+                issuer = cert['issuer']
+                subject = cert['subject']
+                not_before = datetime.strptime(cert['notBefore'], '%b %d %H:%M:%S %Y %Z')
+                not_after = datetime.strptime(cert['notAfter'], '%b %d %H:%M:%S %Y %Z')
+                days_until_expiry = (not_after - datetime.today()).days
+                is_valid = days_until_expiry > 0
+                error = None
+    except ssl.SSLError as e:
+        is_valid = False
+        error = str(e)
+    except socket.error as e:
+        is_valid = False
+        error = str(e)
+    return {
+        'hostname': hostname,
+        'issuer': issuer,
+        'subject': subject,
+        'not_before': not_before,
+        'not_after': not_after,
+        'days_until_expiry': days_until_expiry,
+        'is_valid': is_valid,
+        'error': error
+    }
+
+def check_ssl_for_sites(sites: list) -> list:
+    results = []
+    for site in sites:
+        if site['ssl_check']:
+            results.append(check_ssl(site['url']))
+    return results
+
+if __name__ == '__main__':
+    import yaml
+    with open('config/sites.yaml', 'r') as f:
+        sites = yaml.safe_load(f)
+    results = check_ssl_for_sites(sites['sites'])
+    import json
+    print(json.dumps(results, indent=4))
